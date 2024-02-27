a22da1d1fc78ec9fc09e48e880dd09f66077b75f
Cang <cang.dinh@deutschebahn.com>
2024-02-27 23:36:14 +0100

    Add index

--- src/views/RanglisteView.vue
+++ src/views/RanglisteView.vue
@@ -1,10 +1,12 @@
 <template>
   <h1>{{ header }}</h1>
   <div class="rangliste">
-    <input v-model="newPlayerName" type="text" placeholder="Name" />
-    <input v-model="newSinglesRating" type="text" placeholder="Einzelwertung" />
-    <input v-model="newDoublesRating" type="text" placeholder="Doppelwertung" />
-    <button @click="clickAdd">Add {{newPlayer}} </button>
+    <input v-model="newPlayerName" type="text" placeholder="Name" id="name" />
+    <input v-model="newSinglesRating" type="text" placeholder="Einzelwertung" id="singles" />
+    <input v-model="newDoublesRating" type="text" placeholder="Doppelwertung" id="doubles" />
+    <button v-if="isValidPlayer" class="rangliste_button" @click="clickAdd" id="button">
+      Add {{ newPlayer }}
+    </button>
   </div>
   <RanglisteVue :players="players" />
 </template>
@@ -15,10 +17,10 @@ import type Player from '@/models/Player.js'
 import RanglisteVue from '@/components/rangliste/Rangliste.vue'
 
 const defaultPlayers: Player[] = [
-  { name: 'Cang', singles: '140', doubles: '80' },
-  { name: 'Anton', singles: '150', doubles: '100' },
-  { name: 'Brecher', singles: '160', doubles: '110' },
-  { name: 'Richtiger Otto', singles: '3000', doubles: '3000' }
+  { id: 1, name: 'Cang', singles: '140', doubles: '80' },
+  { id: 2, name: 'Anton', singles: '150', doubles: '100' },
+  { id: 3, name: 'Brecher', singles: '160', doubles: '110' },
+  { id: 4, name: 'Richtiger Otto', singles: '3000', doubles: '3000' }
 ]
 
 const header = ref('Rangliste')
@@ -26,24 +28,39 @@ const newPlayerName = ref('')
 const newSinglesRating = ref('')
 const newDoublesRating = ref('')
 const players = ref<Player[]>([])
+let lastId: number = 0
 
 onMounted(() => {
-  const storedPlayers = getPlayersFromSessionStorage()
-  if (storedPlayers.length > 0) {
-    players.value = storedPlayers
-  } else {
-    players.value = defaultPlayers
-  }
+  const storedPlayers = initPlayerList()
+  saveLastIdToLocalStorage(storedPlayers.length)
+})
+
+const isValidPlayer = computed(() => {
+  return newPlayerName.value && newSinglesRating.value && newDoublesRating.value
 })
 
 const newPlayer = computed(() => {
-  if (newPlayerName.value && newSinglesRating.value && newDoublesRating.value) {
+  if (isValidPlayer) {
     return `${newPlayerName.value}(${newSinglesRating.value}/${newDoublesRating.value})`
   }
-});
+})
+
+function initPlayerList(): Player[] {
+  const storedPlayers = getPlayersFromSessionStorage()
+  if (storedPlayers.length > 0) {
+    console.log('Load stored player list')
+    return (players.value = storedPlayers)
+  } else {
+    console.log('Load default player list')
+    return (players.value = defaultPlayers)
+  }
+}
 
 function clickAdd(): void {
+  const id = createNewPlayerId()
+
   players.value.push({
+    id,
     name: newPlayerName.value,
     singles: newSinglesRating.value,
     doubles: newDoublesRating.value
@@ -52,10 +69,27 @@ function clickAdd(): void {
   clearForm()
 }
 
+function createNewPlayerId(): number {
+  lastId = getLastIdFromLocalStorage()
+  const newPlayerId = lastId + 1
+  saveLastIdToLocalStorage(newPlayerId)
+  return newPlayerId
+}
+
 function clearForm(): void {
   newPlayerName.value = ''
   newSinglesRating.value = ''
   newDoublesRating.value = ''
+}
+
+function getLastIdFromLocalStorage(): number {
+  const storedId = localStorage.getItem('lastId')
+  const jsonStoredId = storedId ? JSON.parse(storedId) : '0'
+  return parseInt(jsonStoredId)
+}
+
+function saveLastIdToLocalStorage(id: number): void {
+  localStorage.setItem('lastId', JSON.stringify(id))
 }
 
 function saveArrayToSessionStorage(key: string, array: any): void {
