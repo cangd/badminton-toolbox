0408109ab5051cb7fc941d42eb1ef41a675af2cb
Tobi <tobias@mittelstaedt-online.de>
2024-03-17 08:56:39 +0000

    Add TeamSelector component, update vitest

---
+++ src/components/rangliste/TeamSelector.spec.ts
@@ -0,0 +1,38 @@
+import { TeamEnum } from '@/models/TeamEnum'
+import { shallowMount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import { type ComponentProps } from 'vue-component-type-helpers'
+import TeamSelector from './TeamSelector.vue'
+
+function setupComponent(overrides: Partial<ComponentProps<typeof TeamSelector>> = {}) {
+  const cut = shallowMount(TeamSelector, {
+    props: {
+      isDisabled: false,
+      teamZugehoerigkeit: TeamEnum.M1,
+      ...overrides
+    }
+  })
+
+  const dropDown = () => cut.find('.teamDropdown_select')
+  const option = () => cut.findAll('.teamDropdown_select--option')
+
+  return {
+    cut,
+    dropDown,
+    option
+  }
+}
+
+describe('TeamSelector.vue ', () => {
+  it('disables select', () => {
+    const { dropDown } = setupComponent({ isDisabled: true })
+
+    expect(dropDown().attributes().disabled).toBeDefined()
+  })
+
+  it('has five options', () => {
+    const { option } = setupComponent({ isDisabled: true })
+
+    expect(option()).toHaveLength(5)
+  })
+})
---
+++ src/models/TeamEnum.ts
@@ -0,0 +1,7 @@
+export enum TeamEnum {
+  M1 = 'M1',
+  M2 = 'M2',
+  M3 = 'M3',
+  M4 = 'M4',
+  E = 'Ersatz'
+}
