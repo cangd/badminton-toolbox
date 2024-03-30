6a9216d010339bae59b2273e19ee3b6b3ea7afe4
Cang <cang.dinh@deutschebahn.com>
2024-03-31 00:58:21 +0100

    Add filter onClick

--- src/helper/rangliste/generateUniquePairshelper.ts
+++ src/helper/rangliste/generateUniquePairshelper.ts
@@ -4,10 +4,19 @@ import type Pair from '../../models/Pair'
 export function generateUniquePairs(players: Player[]): Pair[] {
   let pair: Pair
   const pairs: Pair[] = []
+  let teamId: number = 0
+  let points: number
 
   for (let i = 0; i < players.length - 1; i++) {
     for (let j = i + 1; j < players.length; j++) {
-      pair = { teamId: i, player1: players[i], player2: players[j] }
+      teamId++
+      points = parseInt(players[i].doubles) + parseInt(players[j].doubles)
+      pair = {
+        teamId,
+        player1: players[i],
+        player2: players[j],
+        points
+      }
       pairs.push(pair)
     }
   }
