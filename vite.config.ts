4e055e58afa4e7e21cd91d32662a2a93c3ba3e87
Cang <cang.dinh@deutschebahn.com>
2024-04-03 18:14:45 +0200

    Rename project

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: '/vereins-rangliste/',
+  base: '/badminton-toolbox/',
   plugins: [vue()],
   resolve: {
     alias: {
