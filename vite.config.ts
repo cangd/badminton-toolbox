030f76039162bc333da0e76e002265a9ece1a024
Cang <cang.dinh@deutschebahn.com>
2024-04-03 13:51:42 +0200

    Set base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,8 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 
-module.exports = {
-  base: process.env.NODE_ENV === 'production' ? '/vereins-rangliste/' : '/'
-}
-
 export default defineConfig({
+  base: '/vereins-rangliste/',
   plugins: [vue()],
   resolve: {
     alias: {
