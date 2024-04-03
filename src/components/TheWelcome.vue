210d0093e3ae1bc334dc4f1aea1e8a2a53bdd408
Cang <cang.dinh@deutschebahn.com>
2024-04-03 18:15:12 +0200

    Add starting page

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
 import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
 </script>
@@ -10,51 +7,35 @@ import SupportIcon from './icons/IconSupport.vue'
 <template>
   <WelcomeItem>
     <template #icon>
-      <DocumentationIcon />
+      <v-icon icon="mdi-hand-wave-outline"></v-icon>
     </template>
-    <template #heading>Documentation</template>
-
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
+    <template #heading>Willkommen zur Badminton Toolbox</template>
+    Erste nette Tools sind schon geschrieben worden. Es kommt bestimmt das eine oder andere Tool
+    dazu!
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <ToolingIcon />
+      <v-icon icon="mdi-clipboard-account-outline"></v-icon>
     </template>
-    <template #heading>Tooling</template>
+    <template #heading><RouterLink to="/rangliste">Rangliste</RouterLink></template>
 
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
-      >Cypress Component Testing</a
-    >.
-
+    In der Rangliste können derzeit Spieler*innen erstellt und bearbeitet werden.
     <br />
-
-    More instructions are available in <code>README.md</code>.
+    Der Name, eine Einzel- und Doppelwertung sowie die Mannschaft kann eingetragen werden.
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <EcosystemIcon />
+      <v-icon icon="mdi-table-account"></v-icon>
     </template>
-    <template #heading>Ecosystem</template>
+    <template #heading><RouterLink to="/calc">Simulator</RouterLink></template>
 
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
+    Mit Hilfe dieses Tools werden alle Paarungen aus der erstellten Rangliste simuliert.
+    <br />
+    Hier zu wählt man ein Team aus, das definitiv ein Doppel aufgestellt wird.
+    <br />
+    Alle übriggebliebene Paare werden mit "Wertung" und "Doppel" (HD1/HD2) angezeigt
   </WelcomeItem>
 
   <WelcomeItem>
@@ -62,27 +43,22 @@ import SupportIcon from './icons/IconSupport.vue'
       <CommunityIcon />
     </template>
     <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
+    Alles was hier entwickelt wurde befindet sich noch im Entwicklungszustand, habt bitte Nachsicht.
+    <br />
+    Weitere Ideen, wie Filter, PDF export und import stehen auf der Liste.
+    <br />
+    Ob sie es hier her schaffen, werden wir sehen ;)
+    <br />
+    Falls ihr Anregungen, Featurewünsche, Verbesserungsvorschläge habt, dürfen diese
+    <a href="https://github.com/cangd/badminton-toolbox/issues">hier</a> geposted werden.
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
       <SupportIcon />
     </template>
-    <template #heading>Support Vue</template>
+    <template #heading>Supported mich</template>
 
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+    In dem ihr das Tool benutzt und mir Feedback gebt!
   </WelcomeItem>
 </template>
