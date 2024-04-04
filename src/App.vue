08b15f1d85afdb247116963b0503952499f45fed
Cang <cang.dinh@deutschebahn.com>
2024-04-04 12:09:08 +0200

    Fix bug in app.vue

--- src/App.vue
+++ src/App.vue
@@ -34,14 +34,19 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import { defaultPlayers } from './helper/defaultPlayers'
-import { savePlayersToSessionStorage } from './helper/rangliste/playersStorageHelper'
+import {
+  getPlayersFromSessionStorage,
+  savePlayersToSessionStorage
+} from './helper/rangliste/playersStorageHelper'
 
 const drawer = ref(false)
 
 const title = ref('Home')
 
 onMounted(() => {
-  savePlayersToSessionStorage(defaultPlayers)
+  if (getPlayersFromSessionStorage().length === 0) {
+    savePlayersToSessionStorage(defaultPlayers)
+  }
 })
 </script>
 
