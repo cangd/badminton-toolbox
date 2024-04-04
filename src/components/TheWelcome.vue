e7a61698fe86a08fb980c470f63130fd445caebe
Cang <cang.dinh@deutschebahn.com>
2024-04-04 18:49:51 +0200

    Move doubles simulator, add news

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -7,16 +7,26 @@ import SupportIcon from './icons/IconSupport.vue'
 <template>
   <WelcomeItem>
     <template #icon>
-      <v-icon icon="mdi-hand-wave-outline"></v-icon>
+      <v-icon icon="mdi-hand-wave-outline" />
     </template>
     <template #heading>Willkommen zur Badminton Toolbox</template>
-    Die Seite ist leider noch nicht mobil freundlich. Auf dem Desktop erlebst Du die Tools viel
-    besser :)
+    Mit der Zeit kommt das eine oder andere Tool, das für die Saison(-vorbereitung) nützlich sein
+    könnte.
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
-      <v-icon icon="mdi-clipboard-account-outline"></v-icon>
+      <v-icon icon="mdi-new-box" />
+    </template>
+    <template #heading>Neue Features:</template>
+
+    <li>05.04.2024: Mobile Friendly design</li>
+    <li>03.04.2024: Die Toolbox geht online</li>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <v-icon icon="mdi-clipboard-account-outline" />
     </template>
     <template #heading><RouterLink to="/rangliste">Rangliste</RouterLink></template>
 
@@ -27,13 +37,25 @@ import SupportIcon from './icons/IconSupport.vue'
 
   <WelcomeItem>
     <template #icon>
-      <v-icon icon="mdi-table-account"></v-icon>
+      <v-icon icon="mdi-table-account" />
     </template>
-    <template #heading><RouterLink to="/calc">Simulator</RouterLink></template>
+    <template #heading><RouterLink to="/simulator">Simulator</RouterLink></template>
 
     Im Simulator wählt man ein Team aus, das definitiv im Doppel aufgestellt wird.
     <br />
     Alle übriggebliebene Paare werden mit "Wertung" und "Doppel" (HD1 oder HD2) angezeigt
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <v-icon icon="mdi-map-marker" />
+    </template>
+    <template #heading>Roadmap</template>
+
+    <li>Filter Funktion für Spieler, die in den Simulator übertragen werden sollen</li>
+    <li>Integrierte Ansicht</li>
+    <li>PDF export</li>
+    <li>PDF import</li>
   </WelcomeItem>
 
   <WelcomeItem>
@@ -44,8 +66,6 @@ import SupportIcon from './icons/IconSupport.vue'
     Anregungen, Featurewünsche, Verbesserungsvorschläge sind herzlich willkommen und dürfen mir
     persönlich mitgeteilt oder auf
     <a href="https://github.com/cangd/badminton-toolbox/issues">GitHub</a> geposted werden.
-    <br />
-    Weitere Ideen, wie Filter, PDF export und weitere stehen auf der Liste.
   </WelcomeItem>
 
   <WelcomeItem>
@@ -54,7 +74,7 @@ import SupportIcon from './icons/IconSupport.vue'
     </template>
     <template #heading>Support</template>
 
-    In dem ihr das Tool benutzt und mir Feedback gebt!
+    Verwendet die Tools und gebt mir Feedback!
     <br />
     Die Seite befindet sich im Entwicklungszustand. Bugs sind nicht auszuschließen.
   </WelcomeItem>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import TheWelcome from '@/components/TheWelcome.vue'
-import CalcView from '@/views/CalcView.vue'
+import DoublesSimulator from '@/views/DoublesSimulator.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import RanglisteView from '../views/RanglisteView.vue'
 
@@ -14,18 +14,12 @@ const router = createRouter({
     {
       path: '/rangliste',
       name: 'rangliste',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: RanglisteView
     },
     {
-      path: '/calc',
-      name: 'calc',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: CalcView
+      path: '/simulator',
+      name: 'simulator',
+      component: DoublesSimulator
     }
   ]
 })
