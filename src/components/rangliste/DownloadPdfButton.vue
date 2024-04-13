2bff9d645dac84265dc26571b5a3bf4150c104b6
Cang <cang.dinh@deutschebahn.com>
2024-04-13 23:14:40 +0200

    Fix compile errors

--- src/components/rangliste/DownloadPdfButton.vue
+++ src/components/rangliste/DownloadPdfButton.vue
@@ -18,31 +18,31 @@
 </template>
 
 <script setup lang="ts">
-import { getPlayersFromSessionStorage } from '@/helper/rangliste/playersStorageHelper';
-import pdfMake from 'pdfmake/build/pdfmake';
-import * as pdfFonts from 'pdfmake/build/vfs_fonts';
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
+// import { getPlayersFromSessionStorage } from '@/helper/rangliste/playersStorageHelper';
+// import pdfMake from 'pdfmake/build/pdfmake';
+// import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+// pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
-const generatePdf = () => {
-  const players = getPlayersFromSessionStorage();
-  const docDefinition: any = {
-    content: [
-      { text: 'Vereinsrangliste VfB Hermsdorf', style: 'header' },
-      {
-        table: {
-          headerRows: 1,
-          widths: ['*', '*', '*', '*'],
-          body: [
-            ['Name', 'Einzel', 'Doppel', 'Mannschaft'],
-            ...players.map((player) => [player.name, player.singles, player.doubles, player.team])
-          ]
-        }
-      }
-    ],
-    styles: {
-      header: { fontSize: 18, bold: true, margin: [0, 0, 0, 10] }
-    }
-  };
-  pdfMake.createPdf(docDefinition).download('vereinsrangliste.pdf');
-};
+// const generatePdf = () => {
+//   const players = getPlayersFromSessionStorage();
+//   const docDefinition: any = {
+//     content: [
+//       { text: 'Vereinsrangliste VfB Hermsdorf', style: 'header' },
+//       {
+//         table: {
+//           headerRows: 1,
+//           widths: ['*', '*', '*', '*'],
+//           body: [
+//             ['Name', 'Einzel', 'Doppel', 'Mannschaft'],
+//             ...players.map((player) => [player.name, player.singles, player.doubles, player.team])
+//           ]
+//         }
+//       }
+//     ],
+//     styles: {
+//       header: { fontSize: 18, bold: true, margin: [0, 0, 0, 10] }
+//     }
+//   };
+//   pdfMake.createPdf(docDefinition).download('vereinsrangliste.pdf');
+// };
 </script>
