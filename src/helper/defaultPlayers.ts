d5212f6a03e04f053874321174dd1a146bc9014d
Cang <cangdinh+github@gmail.com>
2024-05-28 12:17:16 +0200

    put no one into simulator per default

--- src/helper/defaultPlayers.ts
+++ src/helper/defaultPlayers.ts
@@ -2,10 +2,10 @@ import type Player from '@/models/Player';
 import { TeamEnum } from '@/models/TeamEnum';
 
 export const defaultPlayers: Player[] = [
-  { id: 1, name: 'Anton', singles: '10', doubles: '30', team: TeamEnum.M1, isInSimulator: true },
-  { id: 2, name: 'Berta', singles: '20', doubles: '10', team: TeamEnum.M1, isInSimulator: true },
-  { id: 3, name: 'Caeser', singles: '30', doubles: '20', team: TeamEnum.M1, isInSimulator: true },
-  { id: 4, name: 'Dora', singles: '40', doubles: '40', team: TeamEnum.M1, isInSimulator: true },
-  { id: 5, name: 'Emil', singles: '50', doubles: '50', team: TeamEnum.M2, isInSimulator: true },
+  { id: 1, name: 'Anton', singles: '10', doubles: '30', team: TeamEnum.M1, isInSimulator: false },
+  { id: 2, name: 'Berta', singles: '20', doubles: '10', team: TeamEnum.M1, isInSimulator: false },
+  { id: 3, name: 'Caeser', singles: '30', doubles: '20', team: TeamEnum.M1, isInSimulator: false },
+  { id: 4, name: 'Dora', singles: '40', doubles: '40', team: TeamEnum.M1, isInSimulator: false },
+  { id: 5, name: 'Emil', singles: '50', doubles: '50', team: TeamEnum.M2, isInSimulator: false },
   { id: 6, name: 'Frieda', singles: '60', doubles: '60', team: TeamEnum.M2, isInSimulator: false }
 ];
